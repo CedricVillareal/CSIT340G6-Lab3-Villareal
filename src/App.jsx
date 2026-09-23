f560b4a6e6fc5c0ba6051108a2f69d37d62cff80
Cedric Villareal <cedricvillareal123@gmail.com>
2026-09-23 08:31:14 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import Total from './Total'
 const App = () => {
   const course = 'CSIT340'
 
-  const part1 = 'Information Management 2'
-  const exercises1 = 3
+  const part1 = {
+  name: 'Information Management 2',
+  exercises: 3
+  }
 
-  const part2 = 'Application Development'
-  const exercises2 = 3
+  const part2 = {
+    name: 'Application Development',
+    exercises: 3
+  }
 
-  const part3 = 'Industry Elective'
-  const exercises3 = 3
+  const part3 = {
+    name: 'Industry Elective',
+    exercises: 3
+  }
 
   return (
     <div>
@@ -20,17 +26,13 @@ const App = () => {
 
       <Content
         part1={part1}
-        exercises1={exercises1}
         part2={part2}
-        exercises2={exercises2}
         part3={part3}
-        exercises3={exercises3}
       />
-
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+      part1={part1}
+      part2={part2}
+      part3={part3}
       />
     </div>
   )
